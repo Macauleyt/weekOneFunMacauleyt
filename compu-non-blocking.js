ab4025a8836b0b16113c86c471acb9198daf44d2
Macauley Treleaven <Macauley@Macauleys-MacBook-Pro.local>
2017-09-29 15:40:43 +0100

    secondcommit

---
+++ compu-non-blocking.js
@@ -0,0 +1,8 @@
+var childProcess = require('child_process')
+
+console.log("Start the process now");
+var newProcess = childProcess.spawn('node', ['fibonacci.js'], {
+    stdio: 'inherit'
+});
+
+console.log("Process Ended");
